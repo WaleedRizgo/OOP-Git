50c534e3d94a830ab49257058c509c19578ebd84
Waleed Rizgo <waleedrizgo@gmail.com>
2020-11-11 13:08:06 +0100

    11.11.2020 um 13

--- CreateFolders/Program.cs
+++ CreateFolders/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace CreateFolders
 {
     class Program
     {
+        public static void CreateFolder(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        public static void CreateFolderArray(string[] paths)
+        {
+            foreach (string folder in paths)
+            {
+                CreateFolder(folder);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateFolder("One Folder Create");
+            string[] folders = {"F1","F2","F3" };
+            CreateFolderArray(folders);
         }
     }
 }
