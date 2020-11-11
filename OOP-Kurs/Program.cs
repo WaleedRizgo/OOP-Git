5d1a2e8b491870abc337c5969fa6450f11d2344c
Waleed Rizgo <waleedrizgo@gmail.com>
2020-11-11 12:03:34 +0100

    HHH

---
+++ OOP-Kurs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP_Kurs
+{
+    class Program
+    {
+        static void printNUM(ref int num)
+        {
+            num = 10;
+            Console.WriteLine(num);
+        }
+        static void Main(string[] args)
+        {
+            int x = 70;
+            Console.WriteLine("X before REF" + x);
+            printNUM(ref x);
+            Console.WriteLine("X after REF" + x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP_out/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP_out
+{
+    class Program
+    {
+        static void printName(out string strName)
+        {
+            strName = "Ahmad";
+            Console.WriteLine("Hallo" +" "+ strName);
+        }
+        static void Main(string[] args)
+        {
+           
+            string myName;
+            printName(out myName);
+            Console.ReadKey();
+        }
+    }
+}
